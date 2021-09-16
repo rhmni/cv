f4c65df705a9448635f3f01caffef15a37366829
Ali Rahmani <alirahmani.py@gmail.com>
2021-09-16 21:41:30 +0430

    hello open source

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <React.Fragment>
+        <App/>
+    </React.Fragment>,
+    document.getElementById('app')
+);
+
+reportWebVitals();
