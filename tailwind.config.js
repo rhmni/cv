99cf6529641c0adcda2f38830b55c26e000e4116
Ali Rahmani <alirahmani.py@gmail.com>
2021-10-01 16:48:02 +0330

    add purge for tailwind and .env_sample

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    purge: [],
+    purge: [
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './src/**/**/*.js',
+        './src/**/**/*.jsx',
+    ],
     darkMode: 'class',
     theme: {
         extend: {
